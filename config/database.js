00befcf2ad8de13cd9d4cb412768f9b7f4962ed1
Dan Scannell <danscannell@gmail.com>
2014-01-10 07:49:41 -0500

    Add partial app skeleton

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url': process.env.PASSPORT_MONGO_LOCAL_URL || process.env.PASSPORT_MONGO_URL
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+// require('./config/passport')(passport);
+
+app.configure(function() {
+	app.use(express.logger('dev'));
+	app.use(express.cookieParser());
+	app.use(express.bodyParser());
+	app.set('view engine', 'ejs');
+	app.use(express.session({secret:'digdug'}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.use(flash());
+});
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('Listening on port ' + port);
